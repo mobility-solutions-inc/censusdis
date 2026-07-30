b4f8b27dd4d053035813fd0deefd5d0593d425af
Connor Riley <connortriley@gmail.com>
2026-07-30 12:31:31 -0400

    Add local government finances dataset symbol

--- censusdis/datasets.py
+++ censusdis/datasets.py
@@ -621,6 +621,8 @@ TIMESERIES_GOVS = "timeseries/govs"
 
 TIMESERIES_GOVSEMP = "timeseries/govsemp"
 
+TIMESERIES_GOVSLOCALFIN = "timeseries/govslocalfin"
+
 TIMESERIES_GOVSPENSION = "timeseries/govspension"
 
 TIMESERIES_GOVSSCHFIN = "timeseries/govsschfin"
@@ -968,6 +970,7 @@ DATASET_REFERENCE_URLS = {
     TIMESERIES_EITS: "http://api.census.gov/data/timeseries/eits/vip",
     TIMESERIES_GOVS: "http://api.census.gov/data/timeseries/govs",
     TIMESERIES_GOVSEMP: "http://api.census.gov/data/timeseries/govsemp",
+    TIMESERIES_GOVSLOCALFIN: "http://api.census.gov/data/timeseries/govslocalfin",
     TIMESERIES_GOVSPENSION: "http://api.census.gov/data/timeseries/govspension",
     TIMESERIES_GOVSSCHFIN: "http://api.census.gov/data/timeseries/govsschfin",
     TIMESERIES_GOVSSTATEFIN: "http://api.census.gov/data/timeseries/govsstatefin",
